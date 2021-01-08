8a915c0bdb12d729db66ce57dfb432a6d1a813ef
ssophiekk <sophie.s.kamil@gmail.com>
2021-01-08 17:54:26 +0000

    configured yaml files

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,11 +1,14 @@
-module "helm_deploy" {
-  source                    = "fuchicorp/chart/helm"
-  deployment_name           = "${var.deployment_name}"
-  deployment_environment    = "${var.deployment_environment}"
-  deployment_endpoint       = "${var.deployment_endpoint[var.deployment_environment]}"
-  deployment_path           = "hello-world"     
-  template_custom_vars = {
-    deployment_image        = "${var.deployment_image}"
-    deployment_environment  = "${var.deployment_environment}"
-  }          
+module "hello-world-deploy" {
+   source  = "fuchicorp/chart/helm"
+   #source  = "tuyalou/chart/helm"
+   deployment_name        = "hello-world"
+   deployment_environment = "${var.deployment_environment}"
+   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
+   deployment_path        = "hello-world"
+   template_custom_vars  = {
+     deployment_image     = "${var.deployment_image}"
+   }
+ }
+output "application_endpoint" {
+    value = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
 }
--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -1,28 +1,22 @@
-variable "deployment_environment" {
-    default = "dev"
-    description = "- (Optional) The namespace to deploy the helm chart."
-}
+variable    "deployment_image" { 
+  default = "docker.fuchicorp.com/hello-world-app-dev-feature:4b7aacb" 
+} 
 
-variable "deployment_name" {
-    default = "hello-world"
-    description = "- (Optional) The name of the deployment."
-}
+variable   "deployment_environment" { 
+    default  = "dev"
+} 
 
-variable "deployment_image" {
-    default = "fsadykov/hello-world:latest"
-    description = "- (Optional) The docker image of the deployment."
-}
+variable   "deployment_endpoint" { 
+    type = "map" 
+    default  = { 
+        test = "test.hello" 
+        dev = "dev.hello" 
+        qa = "qa.hello" 
+        prod = "hello" 
+        stage = "stage.hello" 
+    } 
+} 
 
-variable "deployment_endpoint" {
-    type = "map"
-
-    default = {
-        dev     = "dev.hello.fuchicorp.com"
-        qa      = "qa.hello.fuchicorp.com"
-        prod    = "hello.fuchicorp.com"
-        stage   = "stage.hello.fuchicorp.com"
-    }
-
-    description = "- (Optional) The endpooint of the deployment."
-}
-  
+variable   "google_domain_name" { 
+    default  = "davrononline.com" 
+} 
